3af94fd27fafca9e1d8bf56078be7dccab278e4c
pederhan <pederhan@uio.no>
2022-08-18 09:49:45 +0200

    Refactor mocktraffic.py
    * Implement metaclass singleton pattern
    * Move MockResponse to top-level
    * Add type annotations
    * Use `self` instead of `MockTraffic.__instance`

--- mreg_cli/mocktraffic.py
+++ mreg_cli/mocktraffic.py
@@ -1,57 +1,70 @@
 import json
 import os
+from typing import Any, Dict, List
+from typing_extensions import Self
 from urllib.parse import urlparse, urlencode
+from .types import ResponseLike
+import requests
 
-class MockTraffic(object):
+class Singleton(type):
+    __instances = {}  # type: dict["Singleton", object]
 
-    # Singleton
-    __instance = None
-    def __new__(cls):
-        if MockTraffic.__instance is None:
-            i = MockTraffic.__instance = object.__new__(cls)
-            i.recording = False
-            i.playback = False
-            i.filename = None
-            i.mock_data = None
-            i.line_num = 0
-        return MockTraffic.__instance
+    def __call__(cls: "Singleton", *args, **kwargs) -> object:
+        if cls not in cls.__instances:
+            cls.__instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls.__instances[cls]
 
-    # the __getattr__( ) method redirects calls to the single instance
-    def __getattr__(self, name):
-        print("__getattr__")
-        return getattr(MockTraffic.__instance, name)
+
+class MockResponse:
+    def __init__(self, json_data, status_code, ok, reason):
+        self.json_data = json_data
+        self.status_code = status_code
+        self.ok = ok
+        self.reason = reason
+
+    def json(self, *args, **kwargs):
+        return self.json_data
+
+
+class MockTraffic(metaclass=Singleton):
+    def __init__(self) -> None:
+        self.recording = False
+        self.playback = False
+        self.filename = ""
+        self.mock_data = []  # type: List[dict]
+        self.line_num = 0
 
     """ Start recording http traffic, commands and console output to the given filename.
         Warning! If the file exists, it will be deleted/overwritten. """
     def start_recording(self, filename):
-        i = MockTraffic.__instance
-        i.recording = True
-        i.filename = filename
+
+        self.recording = True
+        self.filename = filename
         try:
             os.remove(filename)
         except:
             pass
 
     """ Prepare to read back commands, http traffic and console output from the given file. """
-    def start_playback(self, filename):
-        i = MockTraffic.__instance
-        i.playback = True
-        i.filename = filename
-        i.line_num = 0
-        f = open(i.filename, 'r')
+
+    def start_playback(self, filename: str):
+        self.playback = True
+        self.filename = filename
+        self.line_num = 0
+        f = open(self.filename, "r")
         lns = f.readlines()
         f.close()
-        i.mock_data = []
+        self.mock_data = []
         for ln in lns:
-            i.mock_data.append(json.loads(ln))
+            self.mock_data.append(json.loads(ln))
 
     def is_recording(self) -> bool:
-        return MockTraffic.__instance.recording
+        return self.recording
 
     def is_playback(self) -> bool:
-        return MockTraffic.__instance.playback
+        return self.playback
 
-    def record_command(self,cmd):
+    def record_command(self, cmd: str) -> None:
         if not self.is_recording():
             return
         # trim spaces, remove comments
@@ -62,20 +75,20 @@ class MockTraffic(object):
         if cmd == '':
             return
         x = {'command':cmd}
-        f = open(MockTraffic.__instance.filename, "a+")
-        f.write("%s\n" % json.dumps(x))
-        f.close()
+        with open(self.filename, "a+") as f:
+            f.write("%s\n" % json.dumps(x))
 
-    def record_output(self,output):
+
+    def record_output(self, output: str):
         if not self.is_recording():
             return
-        x = {'output':output}
-        f = open(MockTraffic.__instance.filename, "a+")
-        f.write("%s\n" % json.dumps(x))
-        f.close()
+        x = {"output": output}
+        with open(self.filename, "a+") as f:
+            f.write("%s\n" % json.dumps(x))
 
     """ Returns only the path + query string components of a url """
-    def urlpath(self, url, params):
+
+    def urlpath(self, url: str, params: Dict[str, str]):
         if params:
             url = f"{url}?{urlencode(params)}"
         up = urlparse(url)
@@ -87,35 +100,46 @@ class MockTraffic(object):
     """ Pretends to perform the http call (method, url and post data),
         verifies that it was the expected http call at this point in time,
         and returns an object that can pass for a http response. """
-    def get_mock_result(self, method, url, params, data):
-        i = MockTraffic.__instance
-        if not i.playback:
+
+    def get_mock_result(
+        self, method: str, url: str, params: Dict[str, str], data: Dict[str, Any]
+    ) -> ResponseLike:
+        if not self.playback:
             raise Exception("Did not call start_playback() before get_mock_result()")
         #
-        i.line_num += 1
-        if i.line_num >= len(i.mock_data):
+        self.line_num += 1
+        if self.line_num >= len(self.mock_data):
             raise Exception("Ran out of mock data, did not expect any more http calls!")
         #
-        obj = i.mock_data[i.line_num-1]
+        obj = self.mock_data[self.line_num - 1]
         method = method.upper()
         url = self.urlpath(url, params)
-        if method != obj['method'] or url != obj['url'] or data != obj['data']:
-            raise Exception("%s(%d):\nExpected: %s %s %s\nDid:      %s %s %s" %
-                (i.filename, i.line_num, obj['method'],obj['url'],obj['data'],method,url,data))
-        #
-        class MockResponse:
-            def __init__(self, json_data, status_code, ok, reason):
-                self.json_data = json_data
-                self.status_code = status_code
-                self.ok = ok
-                self.reason = reason
-
-            def json(self, *args, **kwargs):
-                return self.json_data
+        if method != obj["method"] or url != obj["url"] or data != obj["data"]:
+            raise Exception(
+                "%s(%d):\nExpected: %s %s %s\nDid:      %s %s %s"
+                % (
+                    self.filename,
+                    self.line_num,
+                    obj["method"],
+                    obj["url"],
+                    obj["data"],
+                    method,
+                    url,
+                    data,
+                )
+            )
         return MockResponse(obj.get('json_data',None), obj.get('status',0), obj.get('ok',False), obj.get('reason',''))
 
     """ Records an http call (method, url and postdata) and the response. """
-    def record(self, method, url, params, data, result):
+
+    def record(
+        self,
+        method: str,
+        url: str,
+        params: Dict[str, str],
+        data: Dict[str, Any],
+        result: requests.Response,
+    ) -> None:
         if not self.is_recording():
             return
         x = {
@@ -131,37 +155,48 @@ class MockTraffic(object):
         except:
             if len(result.content)>0:
                 x['body'] = result.content.decode('utf-8')
-        f = open(MockTraffic.__instance.filename, "a+")
-        f.write("%s\n" % json.dumps(x))
-        f.close()
+        with open(self.filename, "a+") as f:
+            f.write("%s\n" % json.dumps(x))
 
     """ Returns the next command from the playback data. """
-    def get_next_command(self):
-        i = MockTraffic.__instance
-        if not i.playback:
+
+    def get_next_command(self) -> None:
+        if not self.playback:
             raise Exception("Did not call start_playback() before get_next_command()")
-        i.line_num += 1
-        if i.line_num >= len(i.mock_data):
+        self.line_num += 1
+        if self.line_num >= len(self.mock_data):
             return None
-        obj = i.mock_data[i.line_num-1]
-        if 'command' not in obj:
-            if 'method' in obj:
-                raise Exception("%s(%d): Expected a http call" % (i.filename,i.line_num))
-            elif 'output' in obj:
-                raise Exception("%s(%d): Expected some output" % (i.filename,i.line_num))
-        return obj['command']
+        obj = self.mock_data[self.line_num - 1]
+        if "command" not in obj:
+            if "method" in obj:
+                raise Exception(
+                    "%s(%d): Expected a http call" % (self.filename, self.line_num)
+                )
+            elif "output" in obj:
+                raise Exception(
+                    "%s(%d): Expected some output" % (self.filename, self.line_num)
+                )
+        return obj["command"]
 
     """ Compares actual console output to what was the expected output at this point """
-    def compare_with_expected_output(self,actual_output):
-        i = MockTraffic.__instance
-        if not i.playback:
+
+    def compare_with_expected_output(self, actual_output: str) -> None:
+        if not self.playback:
             raise Exception("Did not call start_playback() before expect_output()")
-        i.line_num += 1
-        if i.line_num >= len(i.mock_data):
-            raise Exception("Didn't expect any more output after end of script:\n\"%s\"" % actual_output)
-        if not 'output' in i.mock_data[i.line_num-1]:
-            raise Exception("%s: Didn't expect any output on line %d:\n\"%s\"" % (i.filename, i.line_num, actual_output))
-        expected = i.mock_data[i.line_num-1]['output']
+        self.line_num += 1
+        if self.line_num >= len(self.mock_data):
+            raise Exception(
+                'Didn\'t expect any more output after end of script:\n"%s"'
+                % actual_output
+            )
+        if not "output" in self.mock_data[self.line_num - 1]:
+            raise Exception(
+                '%s: Didn\'t expect any output on line %d:\n"%s"'
+                % (self.filename, self.line_num, actual_output)
+            )
+        expected = self.mock_data[self.line_num - 1]["output"]
         if actual_output != expected:
-            raise Exception("%s(%d): The actual output differs from the expected output.\nGot:      \"%s\"\nExpected: \"%s\"" 
-                % (i.filename, i.line_num, actual_output, expected))
+            raise Exception(
+                '%s(%d): The actual output differs from the expected output.\nGot:      "%s"\nExpected: "%s"'
+                % (self.filename, self.line_num, actual_output, expected)
+            )
