61e2d8ae9f0b4ea645dce6ba8ac4324d4805aa29
pederhan <pederhan@uio.no>
2022-08-26 13:50:47 +0200

    Add log.py tests (WIP)

---
+++ tests/test_log.py
@@ -0,0 +1,28 @@
+from typing import Optional
+import pytest
+from pathlib import Path
+from mreg_cli import log
+
+
+@pytest.mark.parametrize("logfile", [None, "test.log"])
+def test__write_log(tmp_path: Path, logfile: Optional[str]) -> None:
+    # Test that no logfile has no effect
+    if logfile is None:
+        log.logfile = None
+        log._write_log("line 1")
+        # Nothing happens
+        return
+
+    f = tmp_path / logfile
+    assert not f.exists()  # File doesn't exist yet
+    log.logfile = str(f)
+
+    # Test that first write creates the file (a+ should create the file)
+    log._write_log("line 1")
+    assert f.read_text() == "line 1\n"
+
+    # Test that subsequent writes append to the file
+    log._write_log("line 2")
+    assert f.read_text() == "line 1\nline 2\n"
+    log._write_log("line 3")
+    assert f.read_text() == "line 1\nline 2\nline 3\n"
